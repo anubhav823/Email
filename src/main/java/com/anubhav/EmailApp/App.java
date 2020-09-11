493f8cfe22100317a740e4fdaf0d0a6f6fe39322
Anubhav Jain <anubhav823@gmail.com>
2020-09-11 21:52:39 +0530

    Commit with rudimentary spring

---
+++ src/main/java/com/anubhav/EmailApp/App.java
@@ -0,0 +1,13 @@
+package com.anubhav.EmailApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
